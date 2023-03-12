87d7ca1e7d737507a023d421a69b6bc701107284
wintyo <wintyo1024@gmail.com>
2023-03-12 12:18:04 +0900

    環境によって出力内容を変えられるようにし、dev環境のデプロイCIを用意する

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import HelloWorld from './components/HelloWorld.vue'
+
+const VITE_APP_TITLE = import.meta.env.VITE_APP_TITLE
 </script>
 
 <template>
+  <div>======== {{ VITE_APP_TITLE }} ========</div>
   <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
